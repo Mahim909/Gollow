549d0dba1f6c5b7723615d1331e21a48cc5dcaab
Mahim909 <fmd006709@gmail.com>
2026-09-19 12:50:42 +0600

    Increase batch size from 5 to 590

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 TARGET_URL = "https://indiefy.me/mahinur-rahman-saif"
 EMAILS_FILE = "emails.txt"
 
-BATCH_SIZE = 5
+BATCH_SIZE = 590
 WAIT_TIME = 60
 
 def get_email_batch(batch_size):
